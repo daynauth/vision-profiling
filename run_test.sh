5069c33386d95839c4477ce2952e90f7efd8abec
Roland Daynauth <daynauth@gmail.com>
2023-07-18 12:21:16 -0400

    run test script

--- run_test.sh
+++ run_test.sh
@@ -1,21 +1,97 @@
-export MEM_SIZE=16
-folder=results/yolos-agx_${MEM_SIZE}gb
-mkdir -p $folder
+export fine=false
+export split=true
 
-cd optimizer 
-python opt_wrapper_mem.py 
-cd ..
-cd optimizer/PLT_latency
-python PYPLT_latency.py
-cp 1/yolos-agx.png ../../${folder}/latency.png
-cd ../..
+#options=(2 4 8 16 ultd)
 
-python /home/roland/ML/vision-profiling/NS-DOT-visualizers/colorer.py \
-/home/roland/ML/vision-profiling/optimizer/testcases/yolos-agx/1/prof.csv \
-/home/roland/ML/vision-profiling/optimizer/testcases/yolos-agx/1/dep.csv \
-/home/roland/ML/vision-profiling/optimizer/testcases/yolos-agx/1/part.csv \
-yolos
+#export MEM_SIZE=2
 
-mv result_DOT_code_yolos.dot ${folder}/graph.dot
+band=($(seq 8 23))
+band+=(0)
 
-cp optimizer/testcases/yolos-agx/1/part.csv ${folder}/part.csv
+run_once(){
+    options=$1
+    export MEM_SIZE=$options
+    for i in "${band[@]}";do
+        cd /home/roland/ML/vision-profiling/
+
+        export BANDWIDTH=$((i*250))
+
+        echo "Running tests with ${BANDWIDTH}mb/s of bandwidth"
+
+            if [ $fine == true ] && [ $split == true ]; then
+                echo "Running tests with fine-grained memory and split memory"
+                cp optimizer/testcases/yolos-agx/1/prof_fine_split.csv optimizer/testcases/yolos-agx/1/prof.csv
+                cp optimizer/testcases/yolos-agx/1/dep_fine_split.csv optimizer/testcases/yolos-agx/1/dep.csv
+                dir=fine_split
+            elif [ $fine == true ] && [ $split == false ]; then
+                echo "Running tests with fine-grained memory and no split memory"
+                cp optimizer/testcases/yolos-agx/1/prof_fine_no_split.csv optimizer/testcases/yolos-agx/1/prof.csv
+                cp optimizer/testcases/yolos-agx/1/dep_fine_no_split.csv optimizer/testcases/yolos-agx/1/dep.csv
+                dir=fine_no_split
+            elif [ $fine == false ] && [ $split == true ]; then
+                echo "Running tests with coarse-grained memory and split memory"
+                cp optimizer/testcases/yolos-agx/1/prof_coarse_split.csv optimizer/testcases/yolos-agx/1/prof.csv
+                cp optimizer/testcases/yolos-agx/1/dep_coarse_split.csv optimizer/testcases/yolos-agx/1/dep.csv
+                dir=coarse_split
+            elif [ $fine == false ] && [ $split == false ]; then
+                echo "Running tests with coarse-grained memory and no split memory"
+                cp optimizer/testcases/yolos-agx/1/prof_coarse_no_split.csv optimizer/testcases/yolos-agx/1/prof.csv
+                cp optimizer/testcases/yolos-agx/1/dep_coarse_no_split.csv optimizer/testcases/yolos-agx/1/dep.csv
+                dir=coarse_no_split
+            else
+                echo "Not a valid configuration"
+                exit 1
+            fi
+
+            
+            folder=results/${dir}/yolos-agx_${MEM_SIZE}gb
+
+            mkdir -p $folder
+
+            cd optimizer 
+            if [ $MEM_SIZE == "ultd" ]; then
+                echo "Running tests with unlimited memory"
+                python opt_wrapper.py
+            else
+                echo "Running tests with ${MEM_SIZE}gb of memory"
+                python opt_wrapper_mem.py
+            fi
+
+
+            #plot latency if i is 0
+            if [ $i == 0 ]; then
+                echo "Plotting latency"
+                cd ..
+                cd optimizer/PLT_latency
+                python PYPLT_latency.py
+                cp 1/yolos-agx.png ../../${folder}/latency.png
+                cd ../..
+
+            else
+                cd ..
+            fi
+
+
+            cp optimizer/testcases/yolos-agx/1/part.csv ${folder}/part.csv
+
+            #if i not 0, plot bandwidth
+            if [ $i != 0 ]; then
+                echo "Plotting bandwidth"
+                cd visualization
+                python render_encoder.py
+                neato -Tpng encoder.dot -o graph.png
+                mv graph.png ../${folder}/graph_${BANDWIDTH}.png
+            else
+                echo "Skipping bandwidth plot"
+            fi
+
+    done
+
+}
+
+option=(2 4 8 16 ultd)
+
+for mem in "${option[@]}";do
+    echo "Running tests with ${mem}gb of memory"
+    run_once $mem
+done
